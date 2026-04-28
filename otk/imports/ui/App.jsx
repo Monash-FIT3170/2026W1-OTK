1dfc5a898e025cbc2022f4f510abc84922bf2e88
Nathan Yeoh <nyeo0003@student.monash.edu>
2026-04-28 23:18:39 +1000

    feat: format UI card template

--- otk/imports/ui/App.jsx
+++ otk/imports/ui/App.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Card from './Card';
 // import { FerociousClaw } from '../engine/card/FerociousClaw';
 
-const card = { name: "ferocious", currentCost: 10, currentAttack: 10, description: 'a nice description'};
+const card = { name: "Ferocious Claw", baseCost: 10, currentCost: 10, currentAttack: 25, baseAttack: 10, description: 'a nice description asdasdasd asd asd asd as d asd asd asd asd as d asd asd asd as dasd  asd asd as d asasdasdasd asd as das dasd asd asd asd asd'};
 
 export const App = () => (
   <div className="page">
-    <h1>Hello</h1>
     <Card cardProps={card} />
   </div>
 );
--- otk/imports/ui/Card.jsx
+++ otk/imports/ui/Card.jsx
@@ -2,30 +2,35 @@ import React from 'react';
 import { motion } from "motion/react";
 
 export default function Card({cardProps}) {
+  const costFontColour = cardProps.currentCost != cardProps.baseCost ? "text-lime-700" : "text-blue-700";
+  const attackFontColour = cardProps.currentAttack != cardProps.baseAttack ? "text-lime-700" : "text-red-700";
+
   return (
     <motion.div >
-      <motion.div whileHover={{scale: 1.1, backgroundColor: "oklch(58.5% 0.233 277.117)"}} 
-      className="flex flex-col gap-3 bg-white rounded-xl shadow-md p-2 aspect-5/7 max-w-60 box-border border-1">
-        <div className="flex flex-1 justify-center box-border border-1">
+      <motion.div whileHover={{scale: 1.1, backgroundColor: "oklch(90.5% 0.233 277.117)"}} 
+      className="flex flex-col gap-2 bg-slate-400 rounded-xl shadow-md p-2 aspect-5/7 min-w-40 min-h-50 max-h-1/7 max-w-1/8 box-border border-slate-600 border-1">
+        <div className="flex flex-2/12 min-h-5 justify-center box-border border-1 bg-slate-300 box-border border-slate-600 border-1 font-mono 
+        font-semibold tracking-wide truncate min-w-0">
           {cardProps.name}
         </div>
-        <div className="flex flex-4 flex-row gap-3">
-          <div className="flex flex-1 flex flex-col">
-              <div className="flex box-border border-1 aspect-square bg-blue-500 rounded-xl justify-center-safe">
-                {cardProps.currentCost}
+        <div className="flex flex-4/10 flex-row gap-3 grow-0">
+          <div className="flex flex-1/6 flex flex-col gap-0.5 grow-0 min-w-0">
+              <div className="flex box-border border-1 border-blue-800 aspect-square bg-blue-300 rounded-xl justify-center-safe font-mono text-ellipsis z-2">
+                <p className={costFontColour}>{cardProps.currentCost} </p>
               </div>
               {cardProps.currentAttack !== null && 
-              <div className="flex box-border border-1 aspect-square bg-red-500 rounded-xl justify-center-safe">
-                {cardProps.currentAttack}
+              <div className="flex box-border border-1 border-red-800 aspect-square bg-red-300 rounded-xl justify-center-safe font-mono text-ellipsis z-2">
+                <p className={attackFontColour}>{cardProps.currentAttack}</p>
               </div>}
           </div>
-          <div className="flex flex-5"> 
-            <img src="https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da842ea3aa5c5df70433b0b5146f" className="flex"/>
+          <div className="flex flex-1/6 box-border border-slate-600 border-1 aspect-5/4"> 
+            <img src="https://assetsio.gnwcdn.com/pokemon-tcg-25th-anniversary-pikachu-base-set-artwork.png?width=690&quality=85&format=jpg&dpr=3&auto=webp"/>
             {/* TODO - Placeholder image */}
           </div>
         </div>
-        <div className="flex flex-5 min-h-30 box-border border-1">
-          {cardProps.description}
+        <div className="flex flex-7/10 min-h-20 box-border border-1 bg-slate-300 box-border border-slate-600 border-1 font-mono 
+        tracking-tighter text-ellipsis overflow-y-clip text-sm grow-3">
+          <p className='ml-1 mt-1'>{cardProps.description}</p>
         </div>
       </motion.div>
 
