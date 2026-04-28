63bc4523137f1c78f2a9db561039c5a14ec745b2
Joshua Gong <joshua03900@gmail.com>
2026-04-28 21:39:08 +1000

    feat: card only shows ATK if attribute is available

--- otk/imports/ui/App.jsx
+++ otk/imports/ui/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 // import { FerociousClaw } from '../engine/card/FerociousClaw';
 
-const card = { name: "ferocious", currentCost: 10, currentAttack: 14, description: 'a nice description'};
+const card = { name: "ferocious", currentCost: 10, currentAttack: 10, description: 'a nice description'};
 
 export const App = () => (
   <div className="page">
--- otk/imports/ui/Card.jsx
+++ otk/imports/ui/Card.jsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 export default function Card({cardProps}) {
   return (
     <motion.div >
-      <motion.div whileHover={{scale: 1.2, backgroundColor: "oklch(58.5% 0.233 277.117)"}} 
+      <motion.div whileHover={{scale: 1.1, backgroundColor: "oklch(58.5% 0.233 277.117)"}} 
       className="flex flex-col gap-3 bg-white rounded-xl shadow-md p-2 aspect-5/7 max-w-60 box-border border-1">
         <div className="flex flex-1 justify-center box-border border-1">
           {cardProps.name}
@@ -14,9 +14,10 @@ export default function Card({cardProps}) {
               <div className="flex box-border border-1 aspect-square bg-blue-500 rounded-xl justify-center-safe">
                 {cardProps.currentCost}
               </div>
+              {cardProps.currentAttack !== null && 
               <div className="flex box-border border-1 aspect-square bg-red-500 rounded-xl justify-center-safe">
                 {cardProps.currentAttack}
-              </div>
+              </div>}
           </div>
           <div className="flex flex-5"> 
             <img src="https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da842ea3aa5c5df70433b0b5146f" className="flex"/>
