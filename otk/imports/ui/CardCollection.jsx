06df708d17c0723949809a8eafd9bb72cbb1ee82
Joshua Gong <joshua03900@gmail.com>
2026-04-28 22:50:22 +1000

    feat: add card hand to display cards

--- otk/imports/ui/CardCollection.jsx
+++ otk/imports/ui/CardCollection.jsx
@@ -1,12 +1,14 @@
 import Card from './Card';
+import { motion } from 'motion/react';
 
-export default function CardCollection(cards) {
+export default function CardCollection({cards}) {
     return (
-        <div className='flex flex-wrap'>
-            {cards.map((card) => (
-                <Card
+        <div className='flex flex-row overflow-x-hidden overflow-y-hidden border rounded-xl p-5 bg-amber-100'>
+            {Object.values(cards).map((card, idx) => (
+                <div key={idx} className={idx !== 0 ? '-ml-10' : ''}>
+                <Card cardProps={card} />
+                </div>
             ))}
         </div>
-    
-    )
+    );
 }
