2a1ad1e411b6db37f62f9ebd68acf257efeaa0f6
Nathan Yeoh <nyeo0003@student.monash.edu>
2026-04-28 19:58:43 +1000

    Base template card

--- otk/imports/ui/Card.jsx
+++ otk/imports/ui/Card.jsx
@@ -1,10 +1,25 @@
 export default function Card(cardProps) {
   return (
     <div>
-      <div className="display-flex flex-direction-column flex">
-        <div>
-          <div>{cardProps.currentAttack}</div>
-          <div>{cardProps.cost}</div>
+      <div className="flex flex-col gap-3 bg-white rounded-xl shadow-md p-6">
+        <div className="flex-1">
+          {cardProps.name}
+        </div>
+        <div className="flex-4 flex-row">
+          <div className="flex-1 flex-col">
+              <div className="flex">
+                {cardProps.currentCost}
+              </div>
+              <div className="flex">
+                {cardProps.currentAttack}
+              </div>
+          </div>
+          <div className="flex-5"> 
+            /** TODO - Placeholder image */
+          </div>
+        </div>
+        <div className="flex-5">
+          {cardProps.description}
         </div>
       </div>
 
