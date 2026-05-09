2c3149485085c31f7567a1bfcaa8236b01c35fb8
Nathan Yeoh <nyeo0003@student.monash.edu>
2026-05-09 21:55:27 +1000

    fix: update rspack to support ts

--- imports/engine/card/CardHand.ts
+++ imports/engine/card/CardHand.ts
@@ -1,10 +1,9 @@
 // CardHand.ts
 
 // importing components
-import { Card, cardData } from './Card';
+import { Card } from './Card';
 
 export class CardHand {
-  // defining attributes
   // defining attributes
   public cards: Card[];
 
@@ -23,7 +22,7 @@ export class CardHand {
   }
 
   returnAllCards(): Card[] {
-    return this.cards.concat(this.cards);
+    return [...this.cards];
   }
 }
 
