71075656031ff509075db962774694c50299357e
ahmadshaqra <ahmad.shaqra1@gmail.com>
2026-05-10 12:29:48 +1000

    fix: updated card registry.

--- imports/engine/card/CardRegistry.ts
+++ imports/engine/card/CardRegistry.ts
@@ -6,23 +6,28 @@ import { Card, cardData } from "./Card";
 export class CardRegistry {
 
   // defining attributes
-  public registry: Record<string, cardData> = {};
+  public registry: Map<string, Card>;
 
   // constructs registry
-  constructor(initialData: Record<string, cardData> = {}) {
-    this.registry = initialData;
+  constructor() {
+    this.registry = new Map()
   }
 
   // add an entry
-  register(cardId: string, value: cardData): void {
-    this.registry[cardId] = value;
+  register(cardId: string, cardObject: Card): void {
+    if (this.registry.has(cardId)) {
+      throw new Error(`Card already registered: ${cardId}`);
+    }
+    this.registry.set(cardId, cardObject);
   }
 
   // get an entry
-  get(cardId: string): cardData | undefined {
-    return this.registry[cardId];
+  get(cardId: string): Card {
+    const cardObject = this.registry.get(cardId);
+    if (!cardObject) {
+      throw new Error(`Unknown cardId: ${cardId}`);
+    }
+    return cardObject;
   }
 
 }
-
-export const cardRegistry = new CardRegistry();
