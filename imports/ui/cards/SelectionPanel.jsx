73c423b3f490df48d5213bf4494f3beb29bd239a
Nathan Yeoh <nyeo0003@student.monash.edu>
2026-05-10 15:48:06 +1000

    feat: card selection panel ui

--- imports/ui/cards/SelectionPanel.jsx
+++ imports/ui/cards/SelectionPanel.jsx
@@ -7,14 +7,27 @@ export function SelectionPanel({ selectionHand }) {
     selectionHand.removeSelection(card);
   };
   return (
-    <div className="flex flex-row overflow-x-hidden overflow-y-hidden border rounded-xl p-5 bg-amber-50 w-full">
-      <div className="flex flex-row justify-center w-full">
-        <p>Select cards</p>
-        {selectionHand.selections.map((card) => (
-          <div key={card.cardId} onClick={() => onSelectedCardClick(card)}>
-            <Card cardProps={card} />
-          </div>
-        ))}
+    <div className ="flex flex-col justify-center">
+      <div className="flex flex-row overflow-x-hidden overflow-y-hidden border rounded-xl p-5 bg-amber-50 w-full">
+        <div className="flex" >
+          <Card cardProps={selectionHand.selectedHand} />
+        </div>
+        <div className="flex flex-col justify-center w-full">
+          <p>Select cards</p>
+          {selectionHand.selections.map((card) => (
+            <div key={card.cardId} onClick={() => onSelectedCardClick(card)}>
+              <Card cardProps={card} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-row justify-center">
+        <div className="flex">
+          <p>Select {selectionHand.minSelectAmount} to {selectionHand.maxSelectAmount} cards</p>
+        </div>
+        <div className="flex">
+          <Button type="button" > ok </Button>
+        </div>
       </div>
     </div>
   );
