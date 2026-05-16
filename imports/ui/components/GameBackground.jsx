3c8bfb406dd4b34917a6db6dad7e6fccc5cc7d77
jwan0518 <2006ayen@gmail.com>
2026-05-17 08:32:30 +1000

    docs[frontend]: Documentation for background display
    Adds a documentation for background/scene naming for display in the future

--- imports/ui/components/GameBackground.jsx
+++ imports/ui/components/GameBackground.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * Displays the background for the game updating with the game stage.
+ * The image used must be named in the format: [background name]-enemy.png and placed in the /assets/environments/ directory.
+ * For example, an Underpass background would require an image named underpass-background.png
+ *
+ * @component
+ * @param {string} backgroundScene - the name of the scene representing the background
+ */
 export function GameBackground({backgroundScene, children}){
   return(
     <div
