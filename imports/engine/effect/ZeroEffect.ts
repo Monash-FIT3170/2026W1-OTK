d0d340a4124b2f3f2fe8cb1dc2f892de9484c0a6
Justin <notjustinla@gmail.com>
2026-05-17 12:48:23 +1000

    refactor[backend]: rename variables in DiscardEffect to match other effect classes

---
+++ imports/engine/effect/ZeroEffect.ts
@@ -0,0 +1,12 @@
+/**
+ * Zero Cost effect sets the current cost of the target cards to 0
+ */
+
+import { GameEngine } from "../GameEngine";
+import { Effect } from "./effect";
+
+export class ZeroEffect implements Effect {
+    resolve(engine: GameEngine, targetCardIndexes?: string[]): void {
+        if (targetCardIndexes) return;
+
+        targetCardIndexes.forEach((id: string) => {
