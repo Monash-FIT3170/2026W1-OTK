270666e441e6220a7c3034a5c8d70878f3d9b3f5
Kayden Nguyen <kayden.nguyen2004@gmail.com>
2026-05-20 22:28:41 +1000

    fix: add absolute layouts for all assets and pixelate the rendering

--- imports/ui/components/PlayerDisplay.jsx
+++ imports/ui/components/PlayerDisplay.jsx
@@ -5,11 +5,12 @@ export const PlayerDisplay = () => {
   const player = useMemo(() => new Player(0, 0), []);
 
   return (
-    <div className="absolute left-24 bottom-30 z-10">
+    <div>
       <img
         src={player.getIdleSpritePath()}
         alt="Main player character"
         className="h-48 w-auto object-contain"
+        style={{ imageRendering: 'pixelated' }}
         draggable={false}
       />
     </div>
