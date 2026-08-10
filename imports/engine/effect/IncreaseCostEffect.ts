fdfb346e7512451bf843e1c490339c60ab608c7a
ahmadshaqra <ahmad.shaqra1@gmail.com>
2026-08-10 18:17:12 +1000

    Implemented AggraRay card and IncreaseCostEffect.

---
+++ imports/engine/effect/IncreaseCostEffect.ts
@@ -0,0 +1,42 @@
+import { GameEngine } from '../GameEngine';
+import { Card } from '../card/Card';
+import { Effect } from './Effect';
+import { cardRegistry } from '../card/CardRegistry';
+
+/**
+ * IncreaseCostEffect increases the cost of one or more cards
+ * 
+ * @author Ahmad Abu-Shaqra
+ * @version 1.0
+ */
+export class IncreaseCostEffect implements Effect {
+  private cards: Card[];
+  private amount: number;
+
+  /**
+   * Effect Constructor
+   * Uses cardRegistry to create cards based on a passed in card
+   * 
+   * @param cards Cards that will have their cost increased
+   * @param amount The value the card costs will be increased by
+   * 
+   * @see Card[]
+   * @see number
+   */
+  constructor(cards: Card[], amount: number) {
+    this.cards = cards;
+    this.amount = amount;
+  }
+
+  /**
+   * Execution of this effect
+   * 
+   * @param engine GameEngine, unused for this effect
+   * @param targetCardIndexes Targeted indices, unused for this effect
+   */
+  resolve(engine: GameEngine, targetCardIndexes?: string[]): void {
+    for (const card of this.cards) {
+        card.currentCost += this.amount
+    }
+  }
+}
