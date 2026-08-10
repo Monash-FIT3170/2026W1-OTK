82b3ead45e04a48564dd3ac295faa821e55762f8
Nathan Yeoh <nyeo0003@student.monash.edu>
2026-08-11 00:45:32 +1000

    Updated card cost balancing

--- imports/engine/card/TaxThePoor.ts
+++ imports/engine/card/TaxThePoor.ts
@@ -1,25 +1,24 @@
-import { Card, cardData} from "./Card";
-import { GameEngine } from "../GameEngine";
-import { cardRegistry } from "./CardRegistry";
-import { SwapCostEffect } from "../effect/SwapCostEffect";
+import { Card, cardData } from './Card';
+import { GameEngine } from '../GameEngine';
+import { cardRegistry } from './CardRegistry';
+import { SwapCostEffect } from '../effect/SwapCostEffect';
 
 export class TaxThePoor extends Card {
-    constructor(data?: Partial<cardData>) {
-        super({
-            cardId: 'tax-the-poor',
-            name: 'Tax the Poor',
-            description: "Adds first card cost to second card",
-            baseCost: 0,
-            currentCost: 0,
-            cardAmountToSelect: {min: 2, max: 2},
-            ...data,
-        });
-    }
+  constructor(data?: Partial<cardData>) {
+    super({
+      cardId: 'tax-the-poor',
+      name: 'Tax the Poor',
+      description: 'Adds first card cost to second card',
+      baseCost: 1,
+      currentCost: 1,
+      cardAmountToSelect: { min: 2, max: 2 },
+      ...data,
+    });
+  }
 
-
-    execute(engine: GameEngine, targetCardIndexes?: string[]): void {
-        new SwapCostEffect().resolve(engine, targetCardIndexes);
-    }
+  execute(engine: GameEngine, targetCardIndexes?: string[]): void {
+    new SwapCostEffect().resolve(engine, targetCardIndexes);
+  }
 }
-    
+
 cardRegistry.register('tax-the-poor', TaxThePoor);
