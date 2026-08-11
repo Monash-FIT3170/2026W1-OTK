204281dabd631ffaa22c754106f43f32cd3e7308
Nathan Yeoh <nyeo0003@student.monash.edu>
2026-08-11 16:02:59 +1000

    fix: fix duplication glitch for card selection

--- imports/ui/cards/DraggableCard.jsx
+++ imports/ui/cards/DraggableCard.jsx
@@ -71,7 +71,7 @@ export function DraggableCard({
           touchAction: 'none',
           cursor: isDragging ? 'grabbing' : 'grab',
         }}
-        whileHover={!isDragging ? { scale: 1.1 } : {}}
+        whileHover={!isDragging ? { scale: 1.1 } : { }}
         onClick={onClick}
         onHoverStart={() => !isDragging && soundManager.playCardHover()}
         onPanStart={handlePanStart}
